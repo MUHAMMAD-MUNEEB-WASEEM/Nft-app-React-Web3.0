ac28fd6d51eaab2a147eaf1afe1e95c4c0840f14
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-12-18 17:31:30 +0500

    Done with NFT app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ function App() {
 
   useEffect(()=>{
     const getMyNfts = async () => {
-      const openSeaData = await axios.get('https://api.opensea.io/api/v1/assets?asset_contract_address=0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB&order_direction=asc')
+      //Main Ethereum Network api
+      // const openSeaData = await axios.get('https://api.opensea.io/api/v1/assets?asset_contract_address=0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB&order_direction=asc')
+      
+      //Rinksbey testNet api
+      
+      const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x8Dd856216f973B9D1E1e764514Fc7B6c714DFED1&order_direction=asc')
+      
       console.log(openSeaData.data.assets)
       setPunkListData(openSeaData.data.assets)
     }
